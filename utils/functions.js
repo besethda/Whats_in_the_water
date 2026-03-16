6868649f2c9d78f21473cae7d6760f2d06af8913
Seth <sdarmistead@gmail.com>
2026-03-16 15:19:41 +0100

    added some styling and utils function

--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,13 @@
+import { Dwellers, Seaplants } from "../data/data"
+
 export const getImageURL = imageName => {
   return new URL(`../src/assets/images/${imageName}`, import.meta.url).href
+}
+
+export const filterDwellersType = (filterBy) => {
+  return Dwellers.filter(e=> Dwellers.type === filterBy)
+}
+
+export const filterSeaplants = (toFilter, filterBy) => {
+  
 }
