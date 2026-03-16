76b37927609f52f75134a7ec8a62cb408648d3d0
Seth <sdarmistead@gmail.com>
2026-03-16 09:10:10 +0100

    added measurements to page

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Content from "./components/Content"
 import Tank from './components/Tank'
-import TankChoices from './components/tankChoices'
+import TankChoices from './components/TankChoices'
 import styles from "./App.module.css"
 
 function App() {
 
   const [tankOption, setTankOption] = useState(null)
   const [tankChoices, setTankChoices] = useState(null)
-  const [tankCondition, setTankCondition] = useState({ph: 7, ammonia: 0, nO:0, nH: 0, n:0, k:0, temperature: 21, })
+  const [tankCondition, setTankCondition] = useState({ph: 7, ammonia: 0, nO:0, nH: 0, n:0, k:0, temperature: 21, light: 8})
 
   return (
     <div className={styles.container}>
       <Header tankChoices={tankChoices}/>
       {!tankOption ? <Tank setTankOption={setTankOption}/> : 
         !tankChoices ? <TankChoices tankOption={tankOption} setTankChoices={setTankChoices}/> :
-        <Content tankOption={tankOption}/>}
+        <Content tankOption={tankOption} tankChoices={tankChoices} tankCondition={tankCondition} setTankCondition={setTankCondition}/>}
       <Footer />
     </div>
   )
